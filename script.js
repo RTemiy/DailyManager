fa3a8673cfa25d2c79e4f79814bd8393f3febc14
RTemiy <83100656+RTemiy@users.noreply.github.com>
2022-07-01 06:14:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const MF= document.getElementById('mf');
+const MF = document.getElementById('mf');
 const Toolbar = document.getElementById('toolbar');
 const SaveButton = document.getElementById('save');
 const NewButton = document.getElementById('new');
@@ -17,25 +17,25 @@ class Event {
 }
 
 function Refresh(lastusedcategory) {
-    Categories.innerHTML='';
-    CategoriesField.innerHTML='';
+    Categories.innerHTML = '';
+    CategoriesField.innerHTML = '';
 
-    for (var x = 0; x < AllCategories.length; x++) {
-        var a = document.createElement('div');
-        a.textContent=AllCategories[x];
-        a.setAttribute('id',AllCategories[x]);
-        a.setAttribute('onclick',`ShowThisCategory('${AllCategories[x]}')`);
-        var b= document.createElement('div');
-        b.setAttribute('id',AllCategories[x]+"F");
+    for (let x = 0; x < AllCategories.length; x++) {
+        let a = document.createElement('div');
+        a.textContent = AllCategories[x];
+        a.setAttribute('id', AllCategories[x]);
+        a.setAttribute('onclick', `ShowThisCategory('${AllCategories[x]}')`);
+        let b = document.createElement('div');
+        b.setAttribute('id', AllCategories[x] + "F");
         Categories.appendChild(a);
         CategoriesField.appendChild(b);
     }
-    for (var x = 0; x < Events.length; x++) {
+    for (let x = 0; x < Events.length; x++) {
         let e = document.createElement('event');
         e.innerHTML = Events[x].beginningDate + ' | <p>' + Events[x].text;
-        e.setAttribute('number',x);
-        e.setAttribute('onclick',`DeleteEvent(${x})`);
-        document.getElementById(Events[x].category+'F').appendChild(e);
+        e.setAttribute('number', x);
+        e.setAttribute('onclick', `EventManager(${x})`);
+        document.getElementById(Events[x].category + 'F').appendChild(e);
     }
 }
 
@@ -44,29 +44,29 @@ var AllCategories = [];
 
 
 function CreateEvent() {
-    var m = document.createElement('NewWindow');
-    var ih = document.createElement('p');
+    let m = document.createElement('NewWindow');
+    let ih = document.createElement('p');
     ih.textContent = 'Введите текст:';
     m.appendChild(ih);
-    var it = document.createElement('input');
+    let it = document.createElement('input');
     m.appendChild(it);
     m.appendChild(document.createElement('hr'));
-    var ch = document.createElement('p');
+    let ch = document.createElement('p');
     ch.textContent = 'Выберите категорию: ';
     m.appendChild(ch);
-    var ic = document.createElement('select');
-    for(var x=0;x<AllCategories.length;x++){
-        var u = document.createElement('option');
-        u.innerText=AllCategories[x];
-        u.value=AllCategories[x];
+    let ic = document.createElement('select');
+    for (let x = 0; x < AllCategories.length; x++) {
+        let u = document.createElement('option');
+        u.innerText = AllCategories[x];
+        u.value = AllCategories[x];
         ic.appendChild(u);
     }
     m.appendChild(ic);
     m.appendChild(document.createElement('hr'));
-    var ok = document.createElement('button');
+    let ok = document.createElement('button');
     ok.textContent = 'Создать';
     m.appendChild(ok);
-    var cancel = document.createElement('button');
+    let cancel = document.createElement('button');
     cancel.textContent = 'Отменить';
     m.appendChild(cancel);
     ok.addEventListener('click', function () {
@@ -82,17 +82,17 @@ function CreateEvent() {
 }
 
 function CreateCategory() {
-    var m = document.createElement('NewWindow');
-    var ih = document.createElement('p');
+    let m = document.createElement('NewWindow');
+    let ih = document.createElement('p');
     ih.innerHTML = 'Введите название категории:';
     m.appendChild(ih);
-    var it = document.createElement('input');
+    let it = document.createElement('input');
     m.appendChild(it);
     m.appendChild(document.createElement('hr'));
-    var ok = document.createElement('button');
+    let ok = document.createElement('button');
     ok.textContent = 'Создать';
     m.appendChild(ok);
-    var cancel = document.createElement('button');
+    let cancel = document.createElement('button');
     cancel.textContent = 'Отменить';
     m.appendChild(cancel);
     ok.addEventListener('click', function () {
@@ -107,42 +107,56 @@ function CreateCategory() {
     MF.appendChild(m);
 }
 
-function ShowThisCategory(a){
-    var b=CategoriesField.childNodes;
-    var c=Categories.childNodes;
-    for(var x=0;x<b.length;x++){
-        if(b[x].id!=a+'F'){
-            c[x].setAttribute('style','color:grey;');
-            b[x].hidden=true;
+function ShowThisCategory(a) {
+    let b = CategoriesField.childNodes;
+    let c = Categories.childNodes;
+    for (let x = 0; x < b.length; x++) {
+        if (b[x].id != a + 'F') {
+            c[x].setAttribute('style', 'color:grey;');
+            b[x].hidden = true;
         }
-        else{
-            c[x].setAttribute('style','color:white;');
-            b[x].hidden=false;
+        else {
+            c[x].setAttribute('style', 'color:white;');
+            b[x].hidden = false;
         }
     };
 }
 
-function SaveData(){
-    localStorage.setItem('Events',JSON.stringify(Events));
-    localStorage.setItem('Categories',JSON.stringify(AllCategories));
+function SaveData() {
+    localStorage.setItem('Events', JSON.stringify(Events));
+    localStorage.setItem('Categories', JSON.stringify(AllCategories));
 }
 
-function LoadData(){
-    Events=JSON.parse(localStorage.getItem('Events'));
-    AllCategories=JSON.parse(localStorage.getItem('Categories'));
+function LoadData() {
+    Events = JSON.parse(localStorage.getItem('Events'));
+    AllCategories = JSON.parse(localStorage.getItem('Categories'));
     Refresh();
 }
 
-function EventManager(o){
-
-}
-
-function DeleteEvent(o){
-    if (confirm(`Удалить событие? — ${Events[o].text}`)){
-        Events.splice(o,1);
+function EventManager(o) {
+    let m = document.createElement('NewWindow');
+    let ih = document.createElement('p');
+    ih.innerHTML = 'Выберите действие с событием:';
+    m.appendChild(ih);
+    let de = document.createElement('button');
+    de.textContent = 'Удалить';
+    de.addEventListener('click', () => {
+        Events.splice(o, 1);
         Refresh();
-    }
+        m.remove();
+    });
+    m.appendChild(de);
+    m.appendChild(document.createElement('p'));
+    let cancel = document.createElement('button');
+    cancel.textContent = 'Отменить действие';
+    m.appendChild(cancel);
+    cancel.addEventListener('click', function () {
+        m.remove();
+    });
+
+    MF.appendChild(m);
 }
+
 
 NewButton.addEventListener('click', CreateEvent, false);
 NewCategory.addEventListener('click', CreateCategory, false);
